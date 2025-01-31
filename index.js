346dabfaa716cb0b0c97af23a3c9f3e87435bbb7
sangza <ucheokenyidm@gmail.com>
2025-01-31 23:20:24 +0100

    correction

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = express();
 const express = require('express');
+const app = express();
+const retrieve = require('./route.js');
+const cors = require('cors')
 
 app.use(cors());
 app.use('/', retrieve);
@@ -7,4 +9,4 @@ app.use(express.json());
 app.use(express.urlencoded({}));
 
 const port = process.env.Port || 3000;
-app.listen(port, console.log('connecting to port 3030', port))
+app.listen(port, console.log('connecting to port ', port))
