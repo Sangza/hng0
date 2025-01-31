36e37ed2527d2f0563b9bfd3a23d80cc685f3e10
sangza <ucheokenyidm@gmail.com>
2025-01-31 23:27:54 +0100

    correction

--- route.js
+++ route.js
@@ -5,7 +5,7 @@ router.get('/', async (req, res)=>{
     const body = {
         "email": "ucheokenyidm@gmail.com",
         "current_datatime":new Date().toISOString(),
-        "github_url":"<https://github.com/Sangza/hng0>"
+        "github_url":"https://github.com/Sangza/hng0"
     }
   res.send(body);
 });
