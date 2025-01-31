70f582cf628a8557ecfddc9b3446006dbd291303
Sangza <ucheokenyidm@gmail.com>
2025-01-31 22:57:26 +0100

    next commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+const express = require("express");
 const app = express();
-const express = require('express');
 
 app.use(express.json());
 app.use(express.urlencoded({}));
 
-
 const port = process.env.Port || 3000;
-app.listen(port, console.log('connecting to port 3030', port))
+app.listen(port, console.log("connecting to port 3030", port));
--- route.js
+++ route.js
@@ -1,13 +1,14 @@
+const express = require("express");
 const router = express.Router();
-const express = require('express');
 
-router.get('/:retrieve', async (req, res)=>{
-    const body = {
-        "email": "ucheokenyidm@gmail.com",
-        "current_datatime":new Date().toISOString(),
-        "github_url":"<https://github.com/sangza/myrepo>"
+router.get("/:retrieve", async (req, res) => {
+  const body = {
+    email: "ucheokenyidm@gmail.com",
+    current_datatime: new Date().toISOString(),
+    github_url: "<https://github.com/sangza/myrepo>",
+  };
 
-    }
+  res.send(body);
+});
 
-    res.send(body);
-})
+exports.module = router;
