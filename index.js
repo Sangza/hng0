d09e31c5a9b152312a87307e47388f40e4e46374
sangza <ucheokenyidm@gmail.com>
2025-01-31 23:42:49 +0100

    second commit

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ app.use('/', retrieve);
 app.use(express.json());
 app.use(express.urlencoded({}));
 
+app.use((req, res) => {
+    res.status(404).json({ error: "Route not found" });
+});
+
 const port = process.env.Port || 3000;
 app.listen(port, console.log('connecting to port ', port))
--- route.js
+++ route.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res)=>{
         "current_datatime":new Date().toISOString(),
         "github_url":"https://github.com/Sangza/hng0"
     }
-  res.send(body);
+  res.status(200).json(body);
 });
 
 module.exports = router;
