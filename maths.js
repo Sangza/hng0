a157ec34f50e28e30c77050c69eedfad14a12f6d
sangza <ucheokenyidm@gmail.com>
2025-02-06 23:59:04 +0100

    sec

--- maths.js
+++ maths.js
@@ -1,86 +1,70 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const getFunFact =  async(number) => {
-try {
+const getFunFact = async (number) => {
+  try {
     const response = await axios.get(`http://numbersapi.com/${number}/math`);
     return response.data;
-} catch (error) {
-    console.error('Error fetching Fun Fact', error.message);
-    return 'Could not retrieve fun fact';
-}
-}
+  } catch (error) {
+    console.error("Error fetching Fun Fact", error.message);
+    return "Could not retrieve fun fact";
+  }
+};
 
+const checkIfPrime = (number) => {
+  if (number < 2) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
 
-const checkIfPrime =(number)=>{
-    if(number < 2) return false;
-    for (let index = 2; index < number; index++) {
-      if(number % index === 0) {
-       return false
-      }else{
-        return true
-      }
-        
+const checkIfComplete = (number) => {
+  if (number < 1) return false;
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (number % i === 0) {
+      sum += i;
     }
-}
-
-const checkIfComplete = (number)=>{
-if(number< 1) return false;
-    let sum = 0 
-
-for (let i = 1; i < number; i++) {
-    if (number % i === 0 ) {
-       sum += i;
-     }   
-}
-return sum === number
-}
-
-const checkifOddOrEven = (number)=>{
-    if(number< 1) return false;
-    if(number % 2 === 0){
-        return "even"
-    }else{
-        return "odd"
-    }
-}
-
-const checkIfArmStrong = (number) => {
-  
-    const digits = number.toString().split('').map(Number);
-    const numDigits = digits.length;
-    const sum = digits.reduce((acc, digit) => acc + Math.pow(digit, numDigits), 0);
-  
-  if(sum === number){
-      return"armstrong"
-  }else{
-      return""
   }
+  return sum === number;
+};
+
+const checkifOddOrEven = (number) => {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
   }
+};
 
-  const checkdigitsum = (number) => {
-    const digits = number.toString().split('').map(Number);
-    const sum = digits.reduce((acc, digit) => acc += digit, 0);
+const checkIfArmstrong = (number) => {
+  const digits = Math.abs(number).toString().split("").map(Number);
+  const numDigits = digits.length;
+  const sum = digits.reduce((acc, digit) => acc + Math.pow(digit, numDigits), 0);
+  return sum === number ? "armstrong" : null;
+};
 
-    return sum;
-  }
+const checkdigitsum = (number) => {
+  const digits = Math.abs(number).toString().split("").map(Number);
+  return digits.reduce((acc, digit) => acc + digit, 0);
+};
 
-  const getProperties = (number) => {
-    const properties = [];
+const getProperties = (number) => {
+  const properties = [];
 
-    const oddOrEven = checkifOddOrEven(number);
-    const armstrong = checkIfArmStrong(number);
+  const armstrong = checkIfArmstrong(number);
+  if (armstrong) properties.push(armstrong);
 
-    if (armstrong) properties.push(armstrong);
-    properties.push(oddOrEven);
+  properties.push(checkifOddOrEven(number));
 
-    return properties;
-}
+  return properties;
+};
+
 module.exports = {
-    getFunFact,
-    checkIfPrime,
-    checkIfComplete,
-    checkifOddOrEven,
-    checkIfArmStrong,
-    checkdigitsum,
-    getProperties
-  };
+  getFunFact,
+  checkIfPrime,
+  checkIfComplete,
+  checkifOddOrEven,
+  getProperties,
+  checkdigitsum,
+};
